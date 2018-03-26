9cfc2742d54cf33643afa36f44cdddbd76f01367
evwilkin <evan.wilkinson@gmail.com>
2018-03-26 12:45:12 -0400

    complete udacimeals app

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reducer from './reducers';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger'
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger)
 );
 
 console.log(store.getState());
